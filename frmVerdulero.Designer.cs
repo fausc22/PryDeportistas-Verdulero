3e15693eb39a945857e6374b7c2165adb06d5af8
root <mgiraudo@ies21.edu.ar>
2023-05-11 12:57:53 -0300

    S

--- frmVerdulero.Designer.cs
+++ frmVerdulero.Designer.cs
@@ -36,12 +36,13 @@
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(298, 53);
+            this.button1.Location = new System.Drawing.Point(244, 65);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -52,7 +53,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(113, 63);
+            this.label1.Location = new System.Drawing.Point(59, 75);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(82, 13);
             this.label1.TabIndex = 1;
@@ -66,7 +67,7 @@
             this.nomb,
             this.Column2,
             this.Column3});
-            this.dgvProductos.Location = new System.Drawing.Point(116, 116);
+            this.dgvProductos.Location = new System.Drawing.Point(12, 111);
             this.dgvProductos.Name = "dgvProductos";
             this.dgvProductos.Size = new System.Drawing.Size(458, 150);
             this.dgvProductos.TabIndex = 3;
@@ -93,16 +94,28 @@
             // 
             // txtPrecio
             // 
-            this.txtPrecio.Location = new System.Drawing.Point(201, 56);
+            this.txtPrecio.Location = new System.Drawing.Point(147, 68);
             this.txtPrecio.Name = "txtPrecio";
             this.txtPrecio.Size = new System.Drawing.Size(81, 20);
             this.txtPrecio.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(87, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "VERDULERÍA";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // frmVerdulero
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(492, 294);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.txtPrecio);
             this.Controls.Add(this.dgvProductos);
             this.Controls.Add(this.label1);
@@ -125,5 +138,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label2;
     }
 }
--- frmVerdulero.cs
+++ frmVerdulero.cs
@@ -73,5 +73,10 @@ namespace PryDeportistas
 
             }
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
